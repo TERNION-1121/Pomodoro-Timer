b82e3def29ad7c8c20ca43f623144a9bc56221d0
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2023-04-04 20:21:50 +0530

    Pomodoro-Timer Done!
    It's done!
    Extra functionalities in mind:
    1. Custom timer
    2. Keybinds

--- pomodoro.py
+++ pomodoro.py
@@ -1,9 +1,9 @@
 import pygame
 
-def fade(**surfacencoords: tuple):
-    for alpha in range(0, 256, 3):
-        for key in surfacencoords:
-            surface, coordinates = surfacencoords[key]
+def fade(*surfacencoords: tuple):
+    for alpha in range(0, 257, 6):
+        for snc in surfacencoords:
+            surface, coordinates = snc
             surface.set_alpha(alpha)
             screen.blit(surface, coordinates)
             pygame.time.delay(30)
@@ -20,40 +20,43 @@ def timer(time: list):
             pygame.time.delay(900)
             timeText = timerFont.render(f"{minute:02d}:{seconds:02d}", False, BG)
             screen.blit(timeText, ((width // 3) - 30, (height // 3) - 30))
-            pygame.display.flip()
+
         if time[1] == 0:
             time[1] = 59
 
-def displayButton(rectColor: tuple, rectCoords: tuple, fontType: pygame.font.Font, fontText: str, fontColor: tuple, fontCoords: tuple, fadeFont = False):
-    pygame.draw.rect(screen, rectColor, rectCoords)
-    font = fontType.render(fontText, False, fontColor)
-    if fadeFont:
-        fade(s1c1 = (font, fontCoords))
-    else:
-        screen.blit(font, fontCoords)
+def displayButton(**buttons: tuple):    # displayButton(item = ((), ()))
+    buttonsToFade = []
+    for button in buttons:
+        rectData, fontData = buttons[button]
+        rectColor, rectCoords = rectData
+        pygame.draw.rect(screen, rectColor, rectCoords)
+        font = fontData[0].render(fontData[1], False, fontData[2])
+        if fontData[4]:
+            buttonsToFade.append((font, fontData[3]))
+        else:
+            screen.blit(font, fontData[3])
     pygame.display.flip()
-
+    fade(*buttonsToFade)
+    
 # basic initilizations/setup
 pygame.init()
 width, height = 768, 432
 screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
 
 # loop variables
-running     =   True
-startScreenFade     =   False
-breakScreenFade  = False
-endScreenFade = False
-status      =   "end"
+running          = True
+startScreenFaded = False
+breakScreenFaded = False
+endScreenFaded   = False
+status           = "begin"
 
 # background and colors
-BG = (223, 235, 247) 
-BLACK = (0, 0, 0)
-GREY = (200, 210, 224)
-DARK_GREY = (110, 112, 120)
-WHITE = (228, 232, 247)
+BG      = (223, 235, 247) 
+BLACK   = (0, 0, 0)
+GREY    = (200, 210, 224)
+DARK_GREY   = (110, 112, 120)
+WHITE       = (228, 232, 247)
 screen.fill(BG)
-pygame.display.flip()
 
 # font setup
 header_font = pygame.font.Font('fonts\LexendGiga-Regular.ttf', 40)
@@ -62,84 +65,139 @@ subheader_font_small = pygame.font.Font('fonts\LexendGiga-ExtraLight.ttf', 40)
 subheader_font_large = pygame.font.Font('fonts\LexendGiga-ExtraLight.ttf', 80)
 button_font = pygame.font.Font('fonts\LexendGiga-Thin.ttf')
 
+# sound setup
+button_select = pygame.mixer.Sound(r"sound_effects\button_select.mp3")
 
 # event loop
-while running:
+while running:  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        mouse = pygame.mouse.get_pos()
 
+        pygame.display.flip()
+        mouse = pygame.mouse.get_pos()
         match status:
 
             case "begin":
-                if not startScreenFade:
-                    mainFont = header_font.render("Pomodoro Timer", False, BLACK)
-                    fade(s1c1 = (mainFont, (178, 128)))
-                    displayButton(GREY, ((width // 2) - 30, 200, 70, 20), button_font, "Ready?", BLACK, (width // 2 - 20, 202), True)
-                    startScreenFade = True
+                rectCoordinates = (width // 2 - 30, 200, 70, 20)
+                textCoordinates = (width // 2 - 20, 202)
 
-                elif (width // 2) - 10 <= mouse[0] <= (width // 2) + 70 and 198 <= mouse[1] <= 218:
-                    displayButton(DARK_GREY, ((width // 2) - 30, 200, 70, 20), button_font, "Ready?", WHITE, (width // 2 - 20, 202))
+                if not startScreenFaded:
+                    mainFont = header_font.render("Pomodoro Timer", False, BLACK)
+                    fade((mainFont, (178, 128)))
+                    pygame.time.delay(1000)
+
+                    displayButton(b = ((GREY, rectCoordinates), (button_font, "Ready?", BLACK, textCoordinates, True)))
+
+                    startScreenFaded = True
+
+                elif (width // 2) - 30 <= mouse[0] <= (width // 2) + 40 and 200 <= mouse[1] <= 220:
+                    displayButton(b = ((DARK_GREY, rectCoordinates), (button_font, "Ready?", WHITE, textCoordinates, False)))
+
                     if event.type == pygame.MOUSEBUTTONDOWN:
+                        pygame.mixer.Sound.play(button_select)
+
                         headerBG = pygame.Surface((450, 50))
                         buttonBG = pygame.Surface((70, 20))
                         headerBG.fill(BG)
                         buttonBG.fill(BG)
-                        displayButton(GREY, (width // 2 - 30, 200, 70, 20), button_font, "Begin!", BLACK, (width // 2 - 20, 202))
-                        fade(s1c1 = (headerBG, (178, 128)), s2c2 = (buttonBG, (width // 2 - 30, 200)))
+
+                        displayButton(b = ((GREY, rectCoordinates), (button_font, "Begin!", BLACK, textCoordinates, False)))
+                        fade((headerBG, (178, 128)), (buttonBG, (width // 2 - 30, 200)))
+
                         status = "ticking"
                 else:
-                    displayButton(GREY, (width // 2 - 30, 200, 70, 20), button_font, "Ready?", BLACK, (width // 2 - 20, 202))
+                    displayButton(b = ((GREY, (width // 2 - 30, 200, 70, 20)), (button_font, "Ready?", BLACK, (width // 2 - 20, 202), False)))
 
             case "ticking":
-                timer([0, 1])
+                timer([30, 0])
+
+                pygame.mixer.music.load(r"sound_effects\argon.mp3")
+                pygame.mixer.music.play(5)
+
                 finishedText = subheader_font_medium.render("Session Completed!", False, BLACK)
-                fade(s1c1 = (finishedText,  (width - 725, height - 300)))
+                fade((finishedText,  (width - 725, height - 300)))
+                pygame.time.delay(1500)
                 status = "break"
             
             case "break":
-                if not breakScreenFade:
-                    displayButton(GREY, (318, 218, 150, 20), button_font, "Begin Break?", BLACK, (345, 220), True)
-                    breakScreenFade = True
+                rectCoordinates = (318, 218, 150, 20)
+                textCoordinates = (340, 220)
+
+                if not breakScreenFaded:
+                    displayButton(b = ((GREY, rectCoordinates), (button_font, "Begin Break?", BLACK, textCoordinates, True)))
+                    breakScreenFaded = True
+
                 elif 318 <= mouse[0] <= 468 and 218 <= mouse[1] <= 238:
-                    displayButton(DARK_GREY, (318, 218, 150, 20), button_font, "Begin Break?", WHITE, (345, 220))
+                    displayButton(b = ((DARK_GREY, rectCoordinates), (button_font, "Begin Break?", WHITE, textCoordinates, False)))
+
                     if event.type == pygame.MOUSEBUTTONDOWN:
+                        pygame.mixer.music.stop()
+                        pygame.mixer.Sound.play(button_select)
+
                         buttonBG = pygame.Surface((150, 20))
                         headerBG = pygame.Surface((750, 60))
                         buttonBG.fill(BG)
                         headerBG.fill(BG)
-                        displayButton(GREY, (318, 218, 150, 20), button_font, "Begin the chill!", BLACK, (335, 220))
-                        fade(s1c1 = (buttonBG, (318, 218)), s2c2 = (headerBG, (30, 145)))
-                        screen.fill(BG)
-                        timer([0, 0])
+                        
+                        displayButton(b = ((GREY, rectCoordinates), (button_font, "Begin the chill!", BLACK, textCoordinates, False)))
+                        fade((buttonBG, (318, 218)), (headerBG, (30, 145)))
+
+                        timer([5, 0])
+
+                        pygame.mixer.music.load(r"sound_effects\argon.mp3")
+                        pygame.mixer.music.play(3)
+
                         status = "end"
                 else:
-                    displayButton(GREY, (318, 218, 150, 20), button_font, "Begin Break?", BLACK, (345, 220))
+                    displayButton(b = ((GREY, rectCoordinates), (button_font, "Begin Break?", BLACK, textCoordinates, False)))
                 
             case "end":
+                rectCoordinates_Button1 = (200, 245, 130, 20)
+                textCoordinates_Button1 = (210, 247)
+
+                rectCoordinates_Button2 = (500, 245, 70, 20)
+                textCoordinates_Button2 = (515, 247)
+
+                screenDummy = pygame.Surface((width, height))
+                screenDummy.fill(BG)
+
                 endFont1 = subheader_font_small.render("Pomodoro Session", False, BLACK)
                 endFont2 = subheader_font_large.render("Complete!", False, BLACK)
-                if not endScreenFade:
-                    fade(sc1 = (endFont1, (160, 100)), sc2 = (endFont2, (140, 130)))
-                    displayButton(GREY, (200, 245, 130, 20), button_font, "Home Screen?", BLACK, (210, 247), True)
-                    displayButton(GREY, (500, 245, 70, 20), button_font, "Quit?", BLACK, (514, 247), True)
-                    endScreenFade = True
+
+                if not endScreenFaded:
+
+                    fade((endFont1, (160, 100)), (endFont2, (140, 130)))
+                    displayButton(b1 = ((GREY, rectCoordinates_Button1), (button_font, "Home Screen?", BLACK, textCoordinates_Button1, True)), b2 = ((GREY, rectCoordinates_Button2), (button_font, "Quit?", BLACK, textCoordinates_Button2, True)))
+                    
+                    endScreenFaded = True
+
                 elif 200 <= mouse[0] <= 330 and 245 <= mouse[1] <= 265:
-                    displayButton(DARK_GREY, (200, 245, 130, 20), button_font, "Home Screen?", WHITE, (210, 247))
+                    displayButton(b = ((DARK_GREY, rectCoordinates_Button1), (button_font, "Home Screen?", WHITE, textCoordinates_Button1, False)))
+
                     if event.type == pygame.MOUSEBUTTONDOWN:
+                        pygame.mixer.Sound.play(button_select)
+
                         startScreenFade = False
                         breakScreenFade = False
                         endScreenFade = False
                         status = "begin"
-                        screen.fill(BG)
+
+                        fade((screenDummy, (0,0)))
+
+                        pygame.mixer.music.stop()
+
                 elif 500 <= mouse[0] <= 570 and 245 <= mouse[1] <= 265:
-                    displayButton(DARK_GREY, (500, 245, 70, 20), button_font, "Quit?", WHITE, (514, 247))
+                    displayButton(b  =((DARK_GREY, rectCoordinates_Button2),(button_font, "Quit?", WHITE, textCoordinates_Button2, False)))
+
                     if event.type == pygame.MOUSEBUTTONDOWN:
+                        pygame.mixer.music.stop()
+                        pygame.mixer.Sound.play(button_select)
+
+                        fade((screenDummy, (0, 0)))
+                        pygame.time.delay(500)
                         running = False
                 else:
-                    displayButton(GREY, (200, 245, 130, 20), button_font, "Home Screen?", BLACK, (210, 247))
-                    displayButton(GREY, (500, 245, 70, 20), button_font, "Quit?", BLACK, (514, 247))
-                
+                    displayButton(b1 = ((GREY, rectCoordinates_Button1), (button_font, "Home Screen?", BLACK, textCoordinates_Button1, False)), b2 = ((GREY, rectCoordinates_Button2), (button_font, "Quit?", BLACK, textCoordinates_Button2, False)))      
                     
 pygame.quit()
