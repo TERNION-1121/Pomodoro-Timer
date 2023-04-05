416d356832025d453745e1793362554454b65fe5
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2023-04-05 20:50:23 +0530

    executable for pomodoro timer
    Next task is to modify the timer, so that it can quit as per the event detected

--- pomodoro.py
+++ pomodoro.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 def fade(*surfacencoords: tuple):
     for alpha in range(0, 257, 6):
@@ -10,7 +11,7 @@ def fade(*surfacencoords: tuple):
         pygame.display.flip()
 
 def timer(time: list):
-    timerFont = pygame.font.Font('fonts\LexendGiga-ExtraLight.ttf', 100)
+    timerFont = pygame.font.Font(working_dir + r'assets\fonts\LexendGiga-ExtraLight.ttf', 100)
     for minute in range(time[0], -1, -1):
         for seconds in range(time[1], -1, -1):
             print(f"{minute:02d}:{seconds:02d}")
@@ -20,11 +21,11 @@ def timer(time: list):
             pygame.time.delay(900)
             timeText = timerFont.render(f"{minute:02d}:{seconds:02d}", False, BG)
             screen.blit(timeText, ((width // 3) - 30, (height // 3) - 30))
-
+            pygame.event.clear()
         if time[1] == 0:
             time[1] = 59
 
-def displayButton(**buttons: tuple):    # displayButton(item = ((), ()))
+def displayButton(**buttons: tuple):
     buttonsToFade = []
     for button in buttons:
         rectData, fontData = buttons[button]
@@ -37,11 +38,15 @@ def displayButton(**buttons: tuple):    # displayButton(item = ((), ()))
             screen.blit(font, fontData[3])
     pygame.display.flip()
     fade(*buttonsToFade)
-    
+
 # basic initilizations/setup
 pygame.init()
+working_dir = os.getcwd().replace(r"\dist", "") + '\\'
 width, height = 768, 432
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Pomodoro Timer")
+icon = pygame.image.load(working_dir + r"assets\Pomodoro_Timer_Icon.png")
+pygame.display.set_icon(icon)
 
 # loop variables
 running          = True
@@ -59,21 +64,20 @@ WHITE       = (228, 232, 247)
 screen.fill(BG)
 
 # font setup
-header_font = pygame.font.Font('fonts\LexendGiga-Regular.ttf', 40)
-subheader_font_medium = pygame.font.Font('fonts\LexendGiga-ExtraLight.ttf', 55)
-subheader_font_small = pygame.font.Font('fonts\LexendGiga-ExtraLight.ttf', 40)
-subheader_font_large = pygame.font.Font('fonts\LexendGiga-ExtraLight.ttf', 80)
-button_font = pygame.font.Font('fonts\LexendGiga-Thin.ttf')
+header_font = pygame.font.Font(working_dir + r'assets\fonts\LexendGiga-Regular.ttf', 40)
+subheader_font_medium = pygame.font.Font(working_dir + r'assets\fonts\LexendGiga-ExtraLight.ttf', 55)
+subheader_font_small = pygame.font.Font(working_dir + r'assets\fonts\LexendGiga-ExtraLight.ttf', 40)
+subheader_font_large = pygame.font.Font(working_dir + r'assets\fonts\LexendGiga-ExtraLight.ttf', 80)
+button_font = pygame.font.Font(working_dir + r'assets\fonts\LexendGiga-Thin.ttf')
 
 # sound setup
-button_select = pygame.mixer.Sound(r"sound_effects\button_select.mp3")
+button_select = pygame.mixer.Sound(working_dir + r"assets\sound_effects\button_select.mp3")
 
 # event loop
 while running:  
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
         pygame.display.flip()
         mouse = pygame.mouse.get_pos()
         match status:
@@ -110,9 +114,9 @@ while running:
                     displayButton(b = ((GREY, (width // 2 - 30, 200, 70, 20)), (button_font, "Ready?", BLACK, (width // 2 - 20, 202), False)))
 
             case "ticking":
-                timer([30, 0])
+                timer([0, 10])
 
-                pygame.mixer.music.load(r"sound_effects\argon.mp3")
+                pygame.mixer.music.load(working_dir + r"assets\sound_effects\argon.mp3")
                 pygame.mixer.music.play(5)
 
                 finishedText = subheader_font_medium.render("Session Completed!", False, BLACK)
@@ -143,9 +147,9 @@ while running:
                         displayButton(b = ((GREY, rectCoordinates), (button_font, "Begin the chill!", BLACK, textCoordinates, False)))
                         fade((buttonBG, (318, 218)), (headerBG, (30, 145)))
 
-                        timer([5, 0])
+                        timer([0, 5])
 
-                        pygame.mixer.music.load(r"sound_effects\argon.mp3")
+                        pygame.mixer.music.load(working_dir + r"assets\sound_effects\argon.mp3")
                         pygame.mixer.music.play(3)
 
                         status = "end"
@@ -199,5 +203,5 @@ while running:
                         running = False
                 else:
                     displayButton(b1 = ((GREY, rectCoordinates_Button1), (button_font, "Home Screen?", BLACK, textCoordinates_Button1, False)), b2 = ((GREY, rectCoordinates_Button2), (button_font, "Quit?", BLACK, textCoordinates_Button2, False)))      
-                    
+    
 pygame.quit()
